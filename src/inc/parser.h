709b77f26c11c4e179d63c81b1eb394f26a42962
Pigioty <Pigiotyreal@gmail.com>
2024-01-28 00:46:36 -0500

    starting parser h

---
+++ src/inc/parser.h
@@ -0,0 +1,19 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include "lexer.h"
+#include <vector>
+#include <memory>
+
+class Parser {
+    public:
+        explicit Parser(const std::vector<Token> &tokens);
+    private:
+        std::vector<Token> tokens;
+        size_t currentTokenIndex;
+
+        const Token &currentToken() const;
+        void advancetoken();
+};
+
+#endif

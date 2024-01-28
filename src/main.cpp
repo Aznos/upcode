99c43c69de50d0f05d5522b97d17172d0fd1fdb8
Pigioty <Pigiotyreal@gmail.com>
2024-01-28 00:10:12 -0500

    args

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,16 @@
 #include <algorithm>
 
 int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        std::cerr << "Usage" << argv[0] << " <filename>.up" << std::endl;
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+    if(!file.is_open()) {
+        std::cerr << "Failed to open file: " << argv[1] << std::endl;
+        return 1;
+    }
+
     std::cout << "hello world!" << std::endl;
 }

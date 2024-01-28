82f7baafd961db2f934482db6675690a38b00876
Pigioty <Pigiotyreal@gmail.com>
2024-01-28 00:08:43 -0500

    includes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 int main(int argc, char* argv[]) {
     std::cout << "hello world!" << std::endl;

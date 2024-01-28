3721567deecb546c94a7de7d68d49ba4dad532c9
Pigioty <Pigiotyreal@gmail.com>
2024-01-28 00:27:29 -0500

    getnNextToken

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -13,3 +13,38 @@ std::vector<Token> Lexer::tokenize() {
 
     return tokens;
 }
+
+Token Lexer::getNextToken() {
+    while(currentPos < sourceCode.length() && isspace(sourceCode[currentPos])) {
+        currentPos++;
+    }
+
+    if(currentPos == sourceCode.length()) {
+        return {TokenType::EndOfLine, ""};
+    }
+
+    char currentChar = sourceCode[currentPos];
+
+    if(isDigit(currentChar)) {
+        return extractNumber();
+    }
+
+    switch(currentChar) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            currentPos++;
+            return {TokenType::Operator, std::string(1, currentChar)};
+        case '(':
+            currentPos++;
+            return {TokenType::OpenParen, "("};
+        case ')':
+            currentPos++;
+            return {TokenType::CloseParen, ")"};
+        default:
+            //TODO: Error out
+            currentPos++;
+            return {TokenType::EndOfLine, ""};
+    }
+}

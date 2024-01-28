1da51de6d4a0e71a859c9764592be85130d02429
Pigioty <Pigiotyreal@gmail.com>
2024-01-28 02:00:32 -0500

    updated eval.h for vars

--- src/inc/eval.h
+++ src/inc/eval.h
@@ -2,10 +2,17 @@
 #define EVAL_H
 
 #include "ast.h"
+#include <unordered_map>
+#include <string>
 
 class Eval {
     public:
         static double eval(const ASTNode* node);
+    private:
+        static std::unordered_map<std::string, double> symbolTable;
+        static double evalASTNode(const ASTNode* node);
+        static double evalVariableDeclaration(const VariableDeclarationNode* node);
+        static double evalVariableUsae(const VariableUsageNode* node);
 };
 
 #endif
